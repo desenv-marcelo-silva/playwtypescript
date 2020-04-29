93e7d368ad2106c5993c4349f1dd0cb550578df7
MacBook Pro <macbookpro@MacBook-Pro-de-Marcelo.local>
2020-04-29 00:05:01 -0300

    chore: ts enum simple example

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -9,12 +9,34 @@ const matches = fs
     return row.split(',');
   });
 
+/*
+ * Javascript way
+
+const homeWin = 'H';
+const awayWin = 'A';
+
+const MatchResult = {
+  HomeWin: 'H',
+  AwayWin: 'A',
+  Draw: 'D',
+};
+*/
+
+/*
+ * Typescript way
+ */
+enum MatchResult {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D',
+}
+
 let manUnitedWins = 0;
 
 for (let match of matches) {
-  if (match[1] === 'Man United' && match[5] === 'H') {
+  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
-  } else if (match[2] == 'Man United' && match[5] === 'A') {
+  } else if (match[2] == 'Man United' && match[5] === MatchResult.AwayWin) {
     manUnitedWins++;
   }
 }
