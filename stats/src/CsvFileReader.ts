03ec2384b2b1ece16db1aeba9f2d780df270d45d
MacBook Pro <macbookpro@MacBook-Pro-de-Marcelo.local>
2020-04-29 00:51:26 -0300

    feat: create abstract class

--- stats/src/CsvFileReader.ts
+++ stats/src/CsvFileReader.ts
@@ -1,10 +1,9 @@
 import fs from 'fs';
-import { dateStringToDate } from './Utils';
 import { MatchResult } from './MatchResult';
 
 type MatchData = [Date, string, string, number, number, MatchResult, string];
 
-export class CsvFileReader {
+export abstract class CsvFileReader {
   data: MatchData[] = [];
 
   constructor(public fileName: string) {}
@@ -21,15 +20,5 @@ export class CsvFileReader {
       .map(this.mapRow);
   }
 
-  mapRow(row: string[]): MatchData {
-    return [
-      dateStringToDate(row[0]),
-      row[1],
-      row[2],
-      parseInt(row[3]),
-      parseInt(row[4]),
-      row[5] as MatchResult,
-      row[6],
-    ];
-  }
+  abstract mapRow(row: string[]): MatchData;
 }
