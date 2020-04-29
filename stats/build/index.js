790bd1fe861a714dca4a427e92211a3add1ea71f
MacBook Pro <macbookpro@MacBook-Pro-de-Marcelo.local>
2020-04-29 00:43:16 -0300

    refactor: create type and a map function

--- stats/build/index.js
+++ stats/build/index.js
@@ -4,22 +4,6 @@ var CsvFileReader_1 = require("./CsvFileReader");
 var MatchResult_1 = require("./MatchResult");
 var reader = new CsvFileReader_1.CsvFileReader('football.csv');
 reader.read();
-console.log(reader.data);
-/*
- * Javascript way
-
-const homeWin = 'H';
-const awayWin = 'A';
-
-const MatchResult = {
-  HomeWin: 'H',
-  AwayWin: 'A',
-  Draw: 'D',
-};
-*/
-/*
- * Typescript way
- */
 var manUnitedWins = 0;
 for (var _i = 0, _a = reader.data; _i < _a.length; _i++) {
     var match = _a[_i];
