77ea41e2b5268561bc63c77db861920ffb9fc7ba
MacBook Pro <macbookpro@MacBook-Pro-de-Marcelo.local>
2020-04-29 22:21:09 -0300

    refactor: using htmlreport

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,6 +1,8 @@
 import { MatchReader } from './MatchReader';
 import { CsvFileReader } from './CsvFileReader';
-import { ConsoleReport } from './reportTargets/ConsoleReport';
+//import { ConsoleReport } from './reportTargets/ConsoleReport';
+import { HtmlReport } from './reportTargets/HtmlReport';
+
 import { WinsAnalysis } from './analyzers/WinsAnalysis';
 import { Summary } from './Summary';
 
@@ -9,6 +11,6 @@ const csvFileReader = new CsvFileReader('football.csv');
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
 
-const summary = new Summary(new WinsAnalysis('Liverpool'), new ConsoleReport());
+const summary = new Summary(new WinsAnalysis('Liverpool'), new HtmlReport());
 
 summary.buildAndPrintReport(matchReader.matches);
