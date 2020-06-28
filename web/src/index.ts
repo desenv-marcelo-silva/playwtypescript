c5c258122f3d49815a3478c4d90eb813425db650
Marcelo Silva (mac) <desenv.marcelo.silva@gmail.com>
2020-06-28 11:54:40 -0300

    feat: disparar eventos Testes com console das funções

--- web/src/index.ts
+++ web/src/index.ts
@@ -2,7 +2,16 @@ import { User } from './models/User';
 
 const user = new User({ name: 'Marcelo', age: 46 });
 
-user.on('clicar', () => {});
-user.on('clicar', () => {});
-user.on('sair', () => {});
+user.on('clicar', () => {
+  console.log('clicar#1');
+});
+user.on('clicar', () => {
+  console.log('clicar#2');
+});
+user.on('sair', () => {
+  console.log('Sair');
+});
+
+user.trigger('clicar');
+user.trigger('sair');
 console.log(user);
