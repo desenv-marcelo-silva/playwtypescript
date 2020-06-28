7dbe34344fc84ba6d01a004ebf7d44a2df616fab
Marcelo Silva (mac) <desenv.marcelo.silva@gmail.com>
2020-06-28 13:03:01 -0300

    feat: funcionalidade save Testes pelo console

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,9 +1,11 @@
 import { User } from './models/User';
 
-const user = new User({ id: 1 });
+//const user = new User({ id: 1 });
 
-user.fetch();
+// user.set({ name: 'Marcelo Silva', age: 56 });
 
-setTimeout(() => {
-  console.log(user);
-}, 4000);
+// user.save();
+
+const user = new User({ name: 'Aline', age: 34 });
+
+user.save();
