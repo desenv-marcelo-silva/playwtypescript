35065f1c310ec7f164b5b548d3b48bbb085c17a5
Marcelo Silva (mac) <desenv.marcelo.silva@gmail.com>
2020-06-28 10:54:33 -0300

    feat: user - propriedades opcionais e update de valores Permite atualizar valores e verificar no console

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,1 +1,12 @@
-console.log('Hello my framework!');
+import { User } from './models/User';
+
+const user = new User({ name: 'Marcelo', age: 46 });
+
+console.log(user.get('name'));
+console.log(user.get('age'));
+
+user.set({ name: 'Marcelo Silva' });
+user.set({ age: 47 });
+
+console.log(user.get('name'));
+console.log(user.get('age'));
