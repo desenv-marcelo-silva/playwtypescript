60cc4768b27f382a04a45fd33be9aa36966fb936
Marcelo Silva (mac) <desenv.marcelo.silva@gmail.com>
2020-06-28 11:10:43 -0300

    feat: criar eventos para User Teste com console no index.

--- web/src/index.ts
+++ web/src/index.ts
@@ -2,11 +2,7 @@ import { User } from './models/User';
 
 const user = new User({ name: 'Marcelo', age: 46 });
 
-console.log(user.get('name'));
-console.log(user.get('age'));
-
-user.set({ name: 'Marcelo Silva' });
-user.set({ age: 47 });
-
-console.log(user.get('name'));
-console.log(user.get('age'));
+user.on('clicar', () => {});
+user.on('clicar', () => {});
+user.on('sair', () => {});
+console.log(user);
