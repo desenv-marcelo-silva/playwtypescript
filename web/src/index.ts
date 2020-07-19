79f7dc93a2f3620f44f1c97584d5cb61bf2504e0
Marcelo Silva (mac) <desenv.marcelo.silva@gmail.com>
2020-07-18 22:01:31 -0300

    build: tsconfig with use strict Null tests must be done

--- web/src/index.ts
+++ web/src/index.ts
@@ -3,6 +3,10 @@ import { User } from './models/User';
 
 const user = User.buildUser({ name: 'Marcelo', age: 46 });
 
-const userForm = new UserForm(document.getElementById('root'), user);
-
-userForm.render();
+const root = document.getElementById('root');
+if (root) {
+  const userForm = new UserForm(root, user);
+  userForm.render();
+} else {
+  throw new Error('Pay attention here. You must select a root element!');
+}
