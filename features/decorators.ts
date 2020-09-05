db116f1c9bad2926bbda3fc70795a4a32c95c7b2
Marcelo Silva (mac) <desenv.marcelo.silva@gmail.com>
2020-09-05 10:58:15 -0300

    chore: decorators in class, accessors, properties and function parameters

--- features/decorators.ts
+++ features/decorators.ts
@@ -1,15 +1,36 @@
+@classDecorator
 class Boat {
+  @testDecorator
   color: string = 'red';
 
+  @testDecorator
   get formattedColor(): string {
     return `This boat color is ${this.color}`;
   }
 
-  @logError('Oops, boat was sunk in ocean...')
-  pilot(): void {
-    throw new Error();
-    console.log('swish');
+  @logError('Something bad!!!')
+  pilot(
+    @parameterDecorator speed: string,
+    @parameterDecorator generateWake: boolean
+  ): void {
+    if (speed === 'fast') {
+      console.log('swish');
+    } else {
+      console.log('nothing!!!');
+    }
   }
+}
+
+function classDecorator(constructor: typeof Boat) {
+  console.log(constructor);
+}
+
+function parameterDecorator(target: any, key: string, index: number) {
+  console.log(key, index);
+}
+
+function testDecorator(target: any, key: string) {
+  console.log(key);
 }
 
 function logError(errorMessage: string) {
@@ -29,5 +50,3 @@ function logError(errorMessage: string) {
     };
   };
 }
-
-new Boat().pilot();
