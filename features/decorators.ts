b512896b07751cee56a49367b3c8b2347376ece7
Marcelo Silva (mac) <desenv.marcelo.silva@gmail.com>
2020-09-05 10:45:11 -0300

    chore: customize decorator - decorator Factory

--- features/decorators.ts
+++ features/decorators.ts
@@ -5,25 +5,28 @@ class Boat {
     return `This boat color is ${this.color}`;
   }
 
-  @logError
+  @logError('Oops, boat was sunk in ocean...')
   pilot(): void {
     throw new Error();
     console.log('swish');
   }
 }
 
-function logError(target: any, key: string, desc: PropertyDescriptor): void {
-  console.log('Target: ', target);
-  console.log('Key: ', key);
-  console.log('desc', desc);
-  const method = desc.value;
+function logError(errorMessage: string) {
+  return function logError(
+    target: any,
+    key: string,
+    desc: PropertyDescriptor
+  ): void {
+    const method = desc.value;
 
-  desc.value = function () {
-    try {
-      method();
-    } catch (error) {
-      console.log('oops, boat was sunk');
-    }
+    desc.value = function () {
+      try {
+        method();
+      } catch (error) {
+        console.log(errorMessage);
+      }
+    };
   };
 }
 
