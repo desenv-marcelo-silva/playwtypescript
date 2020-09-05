c01e57fb302352286c12360095d0e8ce210af943
Marcelo Silva (mac) <desenv.marcelo.silva@gmail.com>
2020-09-05 13:02:23 -0300

    chore: reflect-metadata first example

---
+++ features/metadata.ts
@@ -0,0 +1,16 @@
+import 'reflect-metadata';
+
+const plane = {
+  color: 'red',
+};
+
+Reflect.defineMetadata('note', 'hi there', plane);
+Reflect.defineMetadata('height', 10, plane);
+
+console.log(plane);
+
+const note = Reflect.getMetadata('note', plane);
+const height = Reflect.getMetadata('height', plane);
+
+console.log(note);
+console.log(height);
